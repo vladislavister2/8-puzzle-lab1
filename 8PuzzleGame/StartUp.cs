ddf7add6150cb8fe459f010cbde8243bd182c2ef
vladislavister2 <91146957+vladislavister2@users.noreply.github.com>
2021-09-30 23:39:02 +0300

    Add files via upload

---
+++ 8PuzzleGame/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using _8PuzzleGame.Solvers;
+using System;
+
+namespace _8PuzzleGame
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            // 1,2,5,3,4,0,6,7,8
+            var arr = new int[3, 3] { { 1, 2, 5 }, { 3, 4, 0 }, { 6, 7, 8 } };
+
+            var board = new Board(arr);
+            var startingState = new State(board, null, null, 0);
+
+            var LDFS = new DfsSolver();
+            var measurer = new PerformanceMeasurer();
+            measurer.StartMeasuring();
+            LDFS.Solve(startingState);
+            measurer.StopMeasuring();
+            measurer.PrintResults();
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            var AStar = new AStarSolver();
+            measurer.StartMeasuring();
+            AStar.Solve(startingState);
+            measurer.StopMeasuring();
+            measurer.PrintResults();
+            Console.ReadLine();
+        }
+    }
+}
